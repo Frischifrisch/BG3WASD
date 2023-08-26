78343728e21dadcaa1a85588d30ebc0621c4ddb9
Sourcery AI <>
2023-08-26 10:38:12 +0000

    'Refactored by Sourcery'

--- tools/sigmaker.py
+++ tools/sigmaker.py
@@ -8,6 +8,7 @@ Extremely stupid sig maker script I use for IDA.
 -Run the script
 -Copy the output and check in IDA if you find your desired address and no other.
 """
+
 import re
 
 code = """\
@@ -17,13 +18,15 @@ paste here
 output = ""
 lines = code.splitlines()
 for line in lines:
-    line_match = re.match(r'\.text:[0-9A-F]+ ([0-9A-F][0-9A-F]) ((?:[0-9A-F][0-9A-F] )*).*?$', line)
-    if line_match:
+    if line_match := re.match(
+        r'\.text:[0-9A-F]+ ([0-9A-F][0-9A-F]) ((?:[0-9A-F][0-9A-F] )*).*?$',
+        line,
+    ):
         first_byte = line_match.group(1)
-        output += first_byte + " "
+        output += f"{first_byte} "
         other_bytes = line_match.group(2)
         other_bytes_match = re.findall(r'([0-9A-F][0-9A-F] )', other_bytes)
-        for i in range(len(other_bytes_match)):
+        for _ in range(len(other_bytes_match)):
             output += "?? "
 
 print(output)
